14f6dcdd67433f9a560b45899e41ba1a52e00d2e
LukeZHar <15649@coderacademy.edu.au>
2024-11-20 17:32:37 +1000

    Feat: Example dog class

---
+++ jsclasses/app.js
@@ -0,0 +1,13 @@
+class Dog {
+    constructor(name, breed, age, color) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+}
+
+let odin = new Dog("Odin", "Husky", "4", "Black/White");
+
+console.log(odin);
