fe2691dadc71850efeb6fee287365cbeb1a57e3c
LukeZHar <15649@coderacademy.edu.au>
2024-11-20 22:45:13 +1000

    Feat: Initialising react application

--- jsclasses/app.js
+++ jsclasses/app.js
@@ -4,10 +4,46 @@ class Dog {
         this.breed = breed;
         this.age = age;
         this.color = color;
+        Dog.#numberOfDogs++;
+    }
+    static #numberOfDogs = 0;
+
+    static resetDogCount(){
+        Dog.#numberOfDogs = 0;
     }
 
+    // static set numberOfDogs(newValue){
+    //     return Dog.#numberOfDogs++;
+    // }
+
+    howl() {
+        // console.log(`Awooooo!! I am ${this.name}.`);
+        throw new HowlError(this.name, this.breed);
+    }
 }
 
 let odin = new Dog("Odin", "Husky", "4", "Black/White");
 
-console.log(odin);
+// console.log(odin.howl());
+
+// Create unique Error class
+class HowlError extends Error {
+    constructor(dogName, dogBreed) {
+        //  super() is called to use the Error classes properties/methods
+        super(`${dogName} of ${dogBreed} failed to Howl.`);
+        
+    }
+}
+
+// let newError = new Error();
+try {
+    odin.howl();
+} catch (error) {
+    // throw new HowlError(odin.name, odin.breed);
+    if (HowlError) {
+        console.log("Howl Error Occured");
+    }
+}
+
+Dog.numberOfDogs = 10;
+console.log(Dog.numberOfDogs);
